aee0c26c314ab7ff11c244070e8d082b6861b8a5
David Daou <davidhdaou@gmail.com>
2021-08-03 12:31:05 +0300

    Fixed bug in width of sign in

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import instance from './instance/axios';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import SignIn from './components/SignIn/SignIn';
 
 class App extends Component {
 
   state = {
-    cards: [ { name: 'Album1', description: 'Description 1', nbOfTracks: 5, createdDate: '02/07/21', updatedDate: '07/07/21', _id: 1 },
-      { name: 'Album2', description: 'Description 2', nbOfTracks: 4, createdDate: '02/07/21', updatedDate: '07/07/21', _id: 2 }
+    cards: [{ name: 'Album1', description: 'Description 1', nbOfTracks: 5, createdDate: '02/07/21', updatedDate: '07/07/21', _id: 1 },
+    { name: 'Album2', description: 'Description 2', nbOfTracks: 4, createdDate: '02/07/21', updatedDate: '07/07/21', _id: 2 }
     ]
   }
-  
+
   /*
   componentDidMount(){
     instance.get("/api/albums/getAlbums").then((response) =>{
@@ -31,10 +31,10 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navigation></Navigation>
-          
-            <Route exact path='/Audio-Library' render={()=> <Cards cards={this.state.cards}></Cards>} ></Route>
-            <Route path='/signIn' render={()=> <SignIn></SignIn>}></Route>
-          
+
+          <Route exact path='/Audio-Library' render={() => <Cards cards={this.state.cards}></Cards>} ></Route>
+          <Route path='/signIn' render={() => <SignIn></SignIn>}></Route>
+
         </div>
       </BrowserRouter>
     );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.css';
 import './navigation.css'
 const Navigation = () => {
   return (
-  <Navbar bg="light" fixed="top" >
-    <Container>
-      <Navbar.Brand href="/Audio-Library">Audio Library</Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link as={Link} to= '/Audio-Library'>Home</Nav.Link>
-        <Nav.Link as={Link} to= '/signIn'>Sign in</Nav.Link>
-      </Nav>
-    </Container>
-  </Navbar>)
+    <Navbar bg="light" fixed="top" >
+      <Container>
+        <Navbar.Brand href="/Audio-Library">Audio Library</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to='/Audio-Library'>Home</Nav.Link>
+          <Nav.Link as={Link} to='/signIn'>Sign in</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>)
 }
 
 export default Navigation;
