22cf9b87ab95bdb13f7b44f2ed303f0c3643c947
David Daou <davidhdaou@gmail.com>
2021-08-03 11:34:13 +0300

    Fixed page refreshing upon pressing Nav Link

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import {Link} from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.css';
 import './navigation.css'
@@ -10,8 +11,8 @@ const Navigation = () => {
     <Container>
       <Navbar.Brand href="/Audio-Library">Audio Library</Navbar.Brand>
       <Nav className="me-auto">
-        <Nav.Link href="/Audio-Library">Home</Nav.Link>
-        <Nav.Link href="/signIn">Sign in</Nav.Link>
+        <Nav.Link as={Link} to= '/Audio-Library'>Home</Nav.Link>
+        <Nav.Link as={Link} to= '/signIn'>Sign in</Nav.Link>
       </Nav>
     </Container>
   </Navbar>)
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,21 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './signIn.css';
 import Input from '../Input/Input';
 import 'bootstrap/dist/css/bootstrap.css';
 import Button from 'react-bootstrap/Button';
 import validator from 'validator';
-class SignIn extends Component{
+class SignIn extends Component {
 
-    state= {
+    state = {
         form:
         {
             email:
             {
                 value: "",
                 isValid: false,
-                rules:{
+                rules: {
                     required: true,
-                    email:true
+                    email: true
                 },
                 touched: false
 
@@ -24,7 +24,7 @@ class SignIn extends Component{
             {
                 value: "",
                 isValid: false,
-                rules:{
+                rules: {
                     required: true
                 },
                 touched: false
@@ -34,10 +34,10 @@ class SignIn extends Component{
             {
                 value: "",
                 isValid: false,
-                rules:{
+                rules: {
                     required: true,
-                    minLength: 8   ,
-                    isPassword:true            
+                    minLength: 8,
+                    isPassword: true
                 },
                 touched: false
 
@@ -46,7 +46,7 @@ class SignIn extends Component{
             {
                 value: "",
                 isValid: false,
-                rules:{
+                rules: {
                     required: true,
                     minLength: 8,
                     isPassword: true
@@ -58,65 +58,66 @@ class SignIn extends Component{
         showSignIn: true
     }
 
-    validate(value, rules){
+    validate(value, rules) {
         let isValid = true;
-        
-        if(rules.required){
-            isValid= value.trim() !== "" && isValid;
+
+        if (rules.required) {
+            isValid = value.trim() !== "" && isValid;
         }
-        if(rules.minLength){
+        if (rules.minLength) {
             isValid = (rules.minLength < value.trim().length) && isValid;
         }
-        if(rules.email){
+        if (rules.email) {
             isValid = isValid && validator.isEmail(value);
         }
         return isValid;
     }
 
-    submitHandler= (event) =>{
+    submitHandler = (event) => {
         event.preventDefault();
+        //check password if sign in check if password == confirm password if sign up
         //axios post
     }
 
-    toggleHandler(){
+    toggleHandler() {
         const doesShow = this.state.showSignIn;
         this.setState({ showSignIn: !doesShow });
     }
 
-    inputChangedHandler = (event, inputId) =>{
-        let updatedForm= {...this.state.form};
-        let updatedElement= {...this.state.form[inputId]};
+    inputChangedHandler = (event, inputId) => {
+        let updatedForm = { ...this.state.form };
+        let updatedElement = { ...this.state.form[inputId] };
         updatedElement.value = event.target.value;
-        updatedElement.touched =true;
-        updatedElement.isValid = this.validate(updatedElement.value, updatedElement.rules);       
+        updatedElement.touched = true;
+        updatedElement.isValid = this.validate(updatedElement.value, updatedElement.rules);
         updatedForm[inputId] = updatedElement;
-        this.setState({form: updatedForm});
+        this.setState({ form: updatedForm });
     };
 
-    render(){
-    return (
-    <div className="box">
-        <div className="form">
-            <div className="container">
-                <div className="text-left">
-                    <h1>{this.state.showSignIn ? "Sign In" : "Sign Up"}</h1>
+    render() {
+        return (
+            <div className="box">
+                <div className="form">
+                    <div className="container">
+                        <div className="text-left">
+                            <h1>{this.state.showSignIn ? "Sign In" : "Sign Up"}</h1>
 
-                    <Button  onClick={this.toggleHandler.bind(this)}>Toggle</Button>       
-                </div>
-            </div>
-            <form onSubmit={this.submitHandler}>
-                {this.state.showSignIn ? null : <Input  invalid={!this.state.form.name.isValid && this.state.form.name.touched} type="text" name="name" placeholder="Your Name" label="Name" onChange={(event)=> this.inputChangedHandler(event,"name")}></Input>}
-                <Input invalid={!this.state.form.email.isValid && this.state.form.email.touched} type="text" name="email" placeholder="Your Email" label="Email" onChange={(event)=> this.inputChangedHandler(event,"email")}></Input>    
-                <Input invalid={!this.state.form.password.isValid && this.state.form.password.touched} type="password" name="password" placeholder="Your Password" label="Password" onChange={(event)=> this.inputChangedHandler(event,"password")}></Input>
-                {this.state.showSignIn ? null : <Input invalid={!this.state.form.confirmPassword.isValid && this.state.form.confirmPassword.touched}  type="password" name="confirmPassword" placeholder="Confirm Password" label="Confirm Password" onChange={(event)=> this.inputChangedHandler(event,"confirmPassword")}></Input>}
-                <div className="container">
-                    <div className="text-left">
-                        <Button type="submit" className="btn" onClick={this.submitHandler}>{this.state.showSignIn ? "Sign In" : "Sign Up"}</Button>
+                            <Button onClick={this.toggleHandler.bind(this)}>Sign In/Sign Up</Button>
+                        </div>
                     </div>
+                    <form onSubmit={this.submitHandler}>
+                        {this.state.showSignIn ? null : <Input invalid={!this.state.form.name.isValid && this.state.form.name.touched} type="text" name="name" placeholder="Your Name" label="Name" onChange={(event) => this.inputChangedHandler(event, "name")}></Input>}
+                        <Input invalid={!this.state.form.email.isValid && this.state.form.email.touched} type="text" name="email" placeholder="Your Email" label="Email" onChange={(event) => this.inputChangedHandler(event, "email")}></Input>
+                        <Input invalid={!this.state.form.password.isValid && this.state.form.password.touched} type="password" name="password" placeholder="Your Password" label="Password" onChange={(event) => this.inputChangedHandler(event, "password")}></Input>
+                        {this.state.showSignIn ? null : <Input invalid={!this.state.form.confirmPassword.isValid && this.state.form.confirmPassword.touched} type="password" name="confirmPassword" placeholder="Confirm Password" label="Confirm Password" onChange={(event) => this.inputChangedHandler(event, "confirmPassword")}></Input>}
+                        <div className="container">
+                            <div className="text-left">
+                                <Button type="submit" className="btn" onClick={this.submitHandler}>{this.state.showSignIn ? "Sign In" : "Sign Up"}</Button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            </form>
-        </div>
-    </div>);
+            </div>);
     }
 }
 
